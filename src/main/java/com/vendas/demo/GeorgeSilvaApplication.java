6ba4a7f41c54a3180cd967fab2e5da7d3982d0ae
George Silva <george.silva@parceiros.odontoprev.com.br>
2019-09-15 20:29:38 -0300

    [INICIAL] Commit inicial projeto vendas (Spring Boot).

---
+++ src/main/java/com/vendas/demo/GeorgeSilvaApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorgeSilvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorgeSilvaApplication.class, args);
+	}
+
+}
